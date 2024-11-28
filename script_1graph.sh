44cbe3b7f170bc47198673c7890e31267c75fba0
Louise Budzynski <louise.budzynski@gmail.com>
2024-11-28 19:38:08 +0100

    update notebook

--- script_1graph.sh
+++ script_1graph.sh
@@ -9,13 +9,13 @@ MAXITER=$4
 NBRUN=$5
 
 #local
-#nohup nice -n 19 julia main_epids_1graph_stab.jl ${GAMMA} ${SIG0} ${N} ${MAXITER} ${NBRUN} > fileres_epid_1graph_stab_T${T}_LAM${LAM}_GAM${GAMMA}_N${N}_iter${MAXITER}_SIG0${SIG0}_run${NBRUN} & 
+nohup nice -n 19 julia main_epids_1graph_stab.jl ${GAMMA} ${SIG0} ${N} ${MAXITER} ${NBRUN} > fileres_epid_1graph_stab_T${T}_LAM${LAM}_GAM${GAMMA}_N${N}_iter${MAXITER}_SIG0${SIG0}_run${NBRUN} & 
 
 #Sybil
 #nohup nice -n 19 julia ../julia-1.8.5/bin/main_epids_1graph_stab.jl ${GAMMA} ${SIG0} ${N} ${MAXITER} ${NBRUN} > fileres_epid_1graph_stab_T${T}_LAM${LAM}_GAM${GAMMA}_N${N}_iter${MAXITER}_SIG0${SIG0}_run${NBRUN} & 
 
 #idra
-nohup julia main_epids_1graph_stab.jl ${GAMMA} ${SIG0} ${N} ${MAXITER} ${NBRUN} > fileres_epid_1graph_stab_T${T}_LAM${LAM}_GAM${GAMMA}_N${N}_iter${MAXITER}_SIG0${SIG0}_run${NBRUN} & 
+#nohup julia main_epids_1graph_stab.jl ${GAMMA} ${SIG0} ${N} ${MAXITER} ${NBRUN} > fileres_epid_1graph_stab_T${T}_LAM${LAM}_GAM${GAMMA}_N${N}_iter${MAXITER}_SIG0${SIG0}_run${NBRUN} & 
 
 sleep 2
 
